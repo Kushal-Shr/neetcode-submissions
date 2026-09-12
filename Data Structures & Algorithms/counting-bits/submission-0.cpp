eb10cf7521bce35f2b2ac0c6ad6c3e9231821a30
Kushal Shrestha <133557671+Kushal-Shr@users.noreply.github.com>
2026-09-12 14:24:46 -0500

    Add: counting-bits - submission-0

---
+++ Data Structures & Algorithms/counting-bits/submission-0.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        vector<int> ans(n + 1, 0);
+
+        for (int i = 0; i <= n; i++)
+        {
+            int count = 0;
+
+            for (int j = 0; j < 32; j++)
+                count += (i >> j) & 1;
+
+            ans[i] = count;
+        }
+
+        return ans;
+    }
+};
